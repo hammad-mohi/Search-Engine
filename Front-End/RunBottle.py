2b12aed87e5a1a3d55b30977586e442bc97677cc
deep2 <deep297@live.ca>
2018-09-30 19:51:37 -0400

    Added search history

--- Front-End/RunBottle.py
+++ Front-End/RunBottle.py
@@ -1,8 +1,11 @@
 from bottle import route, run, get, post, request, static_file, template
+from collections import OrderedDict
+
+keywords = {}
 
 @route('/')
 def hello():
-  return template('Main-Page.html',Table="", root='./')
+  return template('Main-Page.html',ResultsTable="", HistoryTable="" ,root='./')
 
 @route('/', method="POST")
 def count_words():
@@ -16,15 +19,30 @@ def count_words():
                 worddict[word] += 1
             else:
                 worddict[word] = 1
-        table = create_table(worddict)
-        return template('Main-Page.html', Table=table)
+            if word in keywords:
+                keywords[word] += 1
+            else:
+                keywords[word] = 1
+        table = create_results_table(worddict)
+        topWordsTable = create_history_table(keywords)
+        return template('Main-Page.html', ResultsTable=table, HistoryTable=topWordsTable)
 
-def create_table(word_dict):
+def create_results_table(word_dict):
     table = '\t<table class="table table-bordered" id="results">\n'
     for word in word_dict:
         table+="\t<tr>\n"
         table+="\t<td>" + word + "</td>\n"
         table+="\t<td>" + str(word_dict[word]) + "</td>\n"
+        table+="\t</tr>\n"
+    table += "\t</table>"
+    return table
+
+def create_history_table(top_words):
+    table = '\t<table class="table table-bordered" id="history">\n'
+    top_words_sorted = sorted(top_words, key=top_words.get, reverse=True)
+    for word in top_words_sorted[:20]:
+        table+="\t<tr>\n"
+        table+="\t<td>" + word + "</td>\n"
         table+="\t</tr>\n"
     table += "\t</table>"
     return table
