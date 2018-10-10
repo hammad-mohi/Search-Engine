0a27cacd577711d0cec094fae28ae1b16e5424f5
deep2 <deep297@live.ca>
2018-10-09 21:41:13 -0400

    History Table now shows when search string is empty

--- Front-End/RunBottle.py
+++ Front-End/RunBottle.py
@@ -13,32 +13,28 @@ def hello():
 def count_words():
     # Get input string from input field and conver to lower case
     inputString = (request.forms.get('keywords')).lower()
-    topWordsTable = ""
 
     # Local dictionary used to store keywords from current search
     worddict = {}
-    # If input string is empty, display error message
-    if(len(inputString) == 0):
-        return template('Main-Page.html', ResultsTable="", HistoryTable=topWordsTable)
-    else:
-        '''
-            Go through every word in the input string and check if it exists
-            in the global keywords dictionary and the local search keywords
-            dictionary. If word exits, increment word count.
-        '''
-        for word in inputString.split():
-            if word in worddict:
-                worddict[word] += 1
-            else:
-                worddict[word] = 1
-            if word in keywords:
-                keywords[word] += 1
-            else:
-                keywords[word] = 1
-        table = create_results_table(worddict)
-        topWordsTable = create_history_table(keywords)
-        # Reload main search page with results and search history tables
-        return template('Main-Page.html', ResultsTable=table, HistoryTable=topWordsTable)
+
+    '''
+        Go through every word in the input string and check if it exists
+        in the global keywords dictionary and the local search keywords
+        dictionary. If word exits, increment word count.
+    '''
+    for word in inputString.split():
+        if word in worddict:
+            worddict[word] += 1
+        else:
+            worddict[word] = 1
+        if word in keywords:
+            keywords[word] += 1
+        else:
+            keywords[word] = 1
+    table = create_results_table(worddict)
+    topWordsTable = create_history_table(keywords)
+    # Reload main search page with results and search history tables
+    return template('Main-Page.html', ResultsTable=table, HistoryTable=topWordsTable)
 
 # Function used to generate HTML results table
 def create_results_table(word_dict):
